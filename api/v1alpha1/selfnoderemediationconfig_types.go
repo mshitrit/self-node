6b22061cfe4668abd66fa79d7416113904281d5f
Michael Shitrit <mshitrit@redhat.com>
2024-04-17 16:52:05 +0300

    Revert "remove default value leftovers"
    This reverts commit 0877d59a7a7494897d30d6eaa7ca0dd4fecd7573.

--- api/v1alpha1/selfnoderemediationconfig_types.go
+++ api/v1alpha1/selfnoderemediationconfig_types.go
@@ -25,9 +25,10 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 const (
-	ConfigCRName                   = "self-node-remediation-config"
-	defaultWatchdogPath            = "/dev/watchdog"
-	defaultIsSoftwareRebootEnabled = true
+	ConfigCRName                         = "self-node-remediation-config"
+	defaultWatchdogPath                  = "/dev/watchdog"
+	DefaultSafeToAssumeNodeRebootTimeout = 180
+	defaultIsSoftwareRebootEnabled       = true
 )
 
 // SelfNodeRemediationConfigSpec defines the desired state of SelfNodeRemediationConfig
@@ -161,8 +162,9 @@ func NewDefaultSelfNodeRemediationConfig() SelfNodeRemediationConfig {
 	return SelfNodeRemediationConfig{
 		ObjectMeta: metav1.ObjectMeta{Name: ConfigCRName},
 		Spec: SelfNodeRemediationConfigSpec{
-			WatchdogFilePath:        defaultWatchdogPath,
-			IsSoftwareRebootEnabled: defaultIsSoftwareRebootEnabled,
+			WatchdogFilePath:                    defaultWatchdogPath,
+			SafeTimeToAssumeNodeRebootedSeconds: DefaultSafeToAssumeNodeRebootTimeout,
+			IsSoftwareRebootEnabled:             defaultIsSoftwareRebootEnabled,
 		},
 	}
 }
